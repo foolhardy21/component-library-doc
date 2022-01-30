1c6a60e51b060a58fceab7acaf44483211e32e24
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-30 09:43:09 +0530

    feat - code snippets added

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const docBtn = document.querySelector('.doc-btn')
 const homeDiv =  document.querySelector('.installation-div')
 const docDiv =  document.querySelector('.documentation-div')
 
+const topScrollBtn =  document.querySelector('.float-btn')
+
+topScrollBtn.addEventListener('click', () => {
+    window.scrollTo(0,0)
+})
+
 homeBtn.addEventListener('click', () => {
     homeDiv.style.display = 'flex'
     docDiv.style.display = 'none'
