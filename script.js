0e037d6476638876d760031ef4e18ad939beb940
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-31 14:36:26 +0530

    feat - landing page update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const homeBtn = document.querySelector('.install-btn')
-const docBtn = document.querySelector('.doc-btn')
+const docBtn = document.querySelectorAll('.doc-btn')
+const docBtnNav = document.querySelector('.doc-btn-nav')
 const homeDiv =  document.querySelector('.setup-div')
 const docDiv =  document.querySelector('.doc-div')
-
-const topScrollBtn =  document.querySelector('.float-btn')
+const topScrollBtn =  document.querySelector('.float-btn') 
+const driftLogo = document.querySelector('#drift-logo') 
 
 topScrollBtn.addEventListener('click', () => {
     window.scrollTo(0,0)
@@ -12,9 +13,25 @@ topScrollBtn.addEventListener('click', () => {
 homeBtn.addEventListener('click', () => {
     homeDiv.style.display = 'flex'
     docDiv.style.display = 'none'
+    homeBtn.style.display = 'none'
+    docBtnNav.style.display = 'block'
 })
 
-docBtn.addEventListener('click', () => {
-    homeDiv.style.display = 'none'
-    docDiv.style.display = 'flex'
+docBtn.forEach((btn => {
+    btn.addEventListener('click', () => {
+        homeDiv.style.display = 'none'
+        docDiv.style.display = 'flex'
+        docBtnNav.style.display = 'none'
+        homeBtn.style.display = 'block'
+    })
+}))
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth <= 900) {
+        homeDiv.classList.remove('flex-row')
+        homeDiv.classList.add('flex-column')
+    } else {
+        homeDiv.classList.remove('flex-column')
+        homeDiv.classList.add('flex-row')
+    }
 })
