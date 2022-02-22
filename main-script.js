22b4bdcd571b214250c05501e33cd04d2730cf6a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-22 22:53:56 +0530

    style - home page ui complete

--- main-script.js
+++ main-script.js
@@ -4,12 +4,8 @@ const mainDoc = document.querySelector('#main-doc')
 
 menuToggleBtn.addEventListener('click', () => {
     asideMenu.classList.remove('dis-hidden')
-    menuToggleBtn.classList.add('dis-hidden')
-    mainDoc.style.background = 'rgb(0,0,0,0.4)'
 })
 
 mainDoc.addEventListener('click', () => {
-    menuToggleBtn.classList.remove('dis-hidden')
     asideMenu.classList.add('dis-hidden')
-    mainDoc.style.background = 'var(--clr-primary)'
 })
