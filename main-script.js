23ed152bdf55ca05a578a3453eb88861fec52bf9
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-22 18:37:17 +0530

    style - menu display toggle added

--- main-script.js
+++ main-script.js
@@ -1,3 +1,20 @@
+const asideMenu = document.querySelector('.aside-menu')
+const menuToggleBtn = document.querySelector('#btn-menu')
+const mainDoc = document.querySelector('#main-doc')
+
+menuToggleBtn.addEventListener('click', () => {
+    asideMenu.classList.remove('dis-hidden')
+    menuToggleBtn.classList.add('dis-hidden')
+    mainDoc.style.background = 'rgb(0,0,0,0.4)'
+})
+
+mainDoc.addEventListener('click', () => {
+    menuToggleBtn.classList.remove('dis-hidden')
+    asideMenu.classList.add('dis-hidden')
+    mainDoc.style.background = 'var(--clr-primary)'
+})
+
+
 // const setupBtn = document.querySelector('.install-btn')
 // const docBtn = document.querySelectorAll('.doc-btn')
 // const logoBtn = document.querySelector('.logo') 
