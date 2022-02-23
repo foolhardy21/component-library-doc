c09e0414e8e98bd6bd9d678b23da834127361141
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-23 15:16:32 +0530

    feat - alerts page complete

--- main-script.js
+++ main-script.js
@@ -1,6 +1,7 @@
 const asideMenu = document.querySelector('.aside-menu')
 const menuToggleBtn = document.querySelector('#btn-menu')
 const mainDoc = document.querySelector('#main-doc')
+const pageSubLinks = document.querySelectorAll('#pg-sublink')
 
 menuToggleBtn.addEventListener('click', () => {
     asideMenu.classList.remove('dis-hidden')
@@ -8,4 +9,17 @@ menuToggleBtn.addEventListener('click', () => {
 
 mainDoc.addEventListener('click', () => {
     asideMenu.classList.add('dis-hidden')
+})
+
+pageSubLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        pageSubLinks.forEach(link2 => {
+            link2.style.borderLeftStyle = 'none'
+            link2.classList.remove('b-primary')
+            link2.classList.remove('pd-left-xs')
+        })
+        link.style.borderLeftStyle = 'solid'
+        link.classList.add('b-primary')
+        link.classList.add('pd-left-xs')
+    })
 })
