b0d4a62e343c94c2f4a6126b68c61aad12048060
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-26 14:05:24 +0530

    rating page complete

---
+++ scripts/rating-script.js
@@ -0,0 +1,15 @@
+const starIcons = document.querySelectorAll('#star-icon')
+
+starIcons.forEach(starIcon => {
+    starIcon.addEventListener('click', (e) => {
+        const maxRating = e.target.dataset.value
+        starIcons.forEach(star => {
+            if(star.dataset.value <= maxRating) {
+                star.classList.add('txt-warn')  
+            } else {
+                star.classList.remove('txt-warn')
+                star.classList.add('txt-off-secondary')  
+            }
+        })
+    })
+})
